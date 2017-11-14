58b85353bb94369b5853c944d3e3e0dd33f71897
douria <douria.zen@gmail.com>
2017-11-14 17:47:13 +0100

    still working on dates

--- app/app.js
+++ app/app.js
@@ -2,4 +2,8 @@ var app = angular.module('myApp', [
     'ngRoute'
 ]);
 
+app.controller('dateController', function ($scope, uibDateParser) {
+    $scope.format = 'yyyy/MM/dd';
+    $scope.date = new Date();
+});
 
