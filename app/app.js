0a7a2035d95c5736c0cdf518f1333c71bfc9c014
douria <douria.zen@gmail.com>
2017-11-28 17:22:30 +0100

    update my code

--- app/app.js
+++ app/app.js
@@ -2,6 +2,12 @@ var app = angular.module('myApp', [
     'ngRoute',
     'ui.bootstrap'
 ]);
+
+// app.controller('close',function($scope,$http){
+//     console.log("heyyyy");
+//     $('#close').modal('hide');// this is bootstrap modal close button id that fire click event
+// });
+
 app.controller('searchResults', function($scope){
     $scope.bddHouses = [
         {photo:'pictures/32477145.png',name:'Gite du Brillant',place:'Nice',nbRoom:'5',shortDesc:'LE meilleur gite de la région'},
@@ -12,7 +18,6 @@ app.controller('searchResults', function($scope){
     ]
    });
 
-<<<<<<< HEAD
 app.service('modalService', function($uibModal,$uibModalStack){
     var modalService = {};
     modalService.openModal = function(url, controller){
@@ -28,6 +33,7 @@ app.service('modalService', function($uibModal,$uibModalStack){
     return modalService;
 });
 
+
 app.controller("connexionController",function($scope,$uibModalInstance){
     $scope.cancelModal = function(){
         $uibModalInstance.dismiss('close');
@@ -37,11 +43,16 @@ app.controller("connexionController",function($scope,$uibModalInstance){
     };
 });
 
-app.service('serviceExample', function(){
-
-    this.service1 = function(){return 1;};
-
+app.controller("registerController",function($scope,$uibModalInstance){
+    $scope.cancelModal = function(){
+        $uibModalInstance.dismiss('close');
+    };
+    $scope.connexion = function(){
+        $uibModalInstance.close('save');
+    };
 });
+
+
 app.controller('locationPage', function($scope){
     $scope.bddHouses = [
         {photo:'32477145.png',name:'Gite du Brillant',place:'Nice',nbRoom:'5',shortDesc:'LE meilleur gite de la région'},
@@ -54,12 +65,14 @@ app.controller('dateController', function ($scope, uibDateParser) {
     $scope.date = new Date();
 });
 
-app.controller('mainController', ['$scope','modalService',function ($scope,modalService, $uibModal,$uibModalStack) {
+app.controller('mainController', ['$scope','modalService','closeService',function ($scope,modalService, $uibModal,$uibModalStack) {
     $scope.open = function(url, controller) {console.log(modalService.openModal);
         modalService.openModal(url, controller);
     };
+    $scope.close = function(url, controller) {console.log(modalService.closeModal);
+        closeService.closeModal(url, controller);
+    };
 }]);
-
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -77,6 +90,9 @@ app.config(function($routeProvider) {
         })
 
 });
+
+// to close thepopups
+
 
 var app = angular.module('sample', [])
     .directive('equalsTo', [function () {
