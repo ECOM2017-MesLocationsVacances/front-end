bd2833d9c5db3a45f2ca94886976a20ab729df59
Unknown <lemaire.timothee@gmail.com>
2017-12-15 01:36:40 +0100

    Un jolie bouton paypal, sur lequel on peut payer ! (mode sandbox)

--- app/app.js
+++ app/app.js
@@ -306,7 +306,58 @@ app.controller('reservationController', function($scope,selectedRoom,selectedDat
         $scope.startDate = selectedDates.getStart();
         $scope.endDate = selectedDates.getEnd();
         $scope.room = selectedRoom.get();
+
+        // Render the PayPal button
+        $('https://www.paypalobjects.com/api/checkout.js',function() {
+            paypal.Button.render({
+
+                // Set your environment
+
+                env: 'sandbox', // sandbox | production
+
+                // Specify the style of the button
+
+                style: {
+                    label: 'paypal',
+                    fundingicons: true, // optional
+                    branding: true, // optional
+                    size:  'medium', // small | medium | large | responsive
+                    shape: 'pill',   // pill | rect
+                    color: 'silver'   // gold | blue | silver | black
+                },
+
+                // PayPal Client IDs - replace with your own
+                // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+
+                client: {
+                    sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+                    production: '<insert production client id>'
+                },
+
+                // Wait for the PayPal button to be clicked
+
+                payment: function(data, actions) {
+                    return actions.payment.create({
+                        transactions: [
+                            {
+                                amount: { total: '0.01', currency: 'USD' }
+                            }
+                        ]
+                    });
+                },
+
+                // Wait for the payment to be authorized by the customer
+
+                onAuthorize: function(data, actions) {
+                    return actions.payment.execute().then(function() {
+                        window.alert('Payment Complete!');
+                    });
+                }
+
+            }, '#paypal-button-container');
+        })
     }
+
 });
 
 
